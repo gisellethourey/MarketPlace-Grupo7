984eddb68f8701ccb73853674b0f23604e114781
Giselle Thourey <thoureygi@gmail.com>
2024-09-24 20:44:01 -0300

    prueba local

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as , Route, Routes } from 'react-router-dom';
 import LandingPage from './views/LandingPage';
 import { AuthProvider } from './context/AuthContext';
 import Perfil from './views/Perfil';
